81c9341a7397c1ba12a9dc1d22a4f8be93855d1b
Mazin Fouad <m.fouad@live.de>
2022-08-28 11:55:46 +0200

    added variables.js

--- script.js
+++ script.js
@@ -1,29 +1,3 @@
-let pokemonsLimit = 29;
-let pokemon;
-let allPokemons = [];
-let pokemonContainer = document.getElementById('pokemon_container');
-let colors = {
-  normal: '#A8A77A',
-  fire: '#EE8130',
-  water: '#6390F0',
-  electric: '#F7D02C',
-  grass: '#7AC74C',
-  ice: '#96D9D6',
-  fighting: '#C22E28',
-  poison: '#A33EA1',
-  ground: '#E2BF65',
-  flying: '#A98FF3',
-  psychic: '#F95587',
-  bug: '#A6B91A',
-  rock: '#B6A136',
-  ghost: '#735797',
-  dragon: '#6F35FC',
-  dark: '#705746',
-  steel: '#B7B7CE',
-  fairy: '#D685AD',
-};
-let mainTypes = Object.keys(colors);
-
 async function renderPokemons() {
   for (let i = 1; i < pokemonsLimit; i++) {
     await getPokemonsData(i);
@@ -71,13 +45,3 @@ function closePopup() {
   document.getElementById('pokemon_popup').classList.add('d-none');
 }
 
-
-/*  todoS
-function filterPokemons() {
-  let query = document.getElementById('search_field').value;
-  let filtedResults = allPokemons.filter(function (name) {
-    return name.allPokemons.includes(query);
-  });
-  console.log(filtedResults);
-} 
- */
